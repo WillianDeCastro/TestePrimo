be91f1808d126a02e71778af8709284ee013fc8c
willian <willian@coltechs.com>
2023-04-20 21:15:43 -0300

    inclusao

--- sacolao/index.js
+++ sacolao/index.js
@@ -4,141 +4,150 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
- 
 
-app.get("/sacolao", async (req,res) =>{
 
-var fruta = "moranGO";
-var frutas = [];
-var qtt = 7;
+app.get("/sacolao", async (req, res) => {
 
-for (let index = 0; index < qtt; index++) {
-     frutas.push(fruta.toLowerCase());
-}
-  res.json(frutas);
+     //var fruta = "moranGO";
+     var frutas = ["morango", "abacaxi", "mamao", "maça", "banana", "melancia", "jaca", "jabuticaba", "lichia"];
+     // var qtt = 7;
+
+     // for (let index = 0; index < qtt; index++) {
+     //      frutas.push(fruta.toLowerCase());
+     // }
+     res.json(frutas);
 });
 
 
-app.get("/compras", async (req,res) =>{
+app.get("/compras", async (req, res) => {
 
-  var compra = "R$ 15,90";
-  var compras = [];
-  var qtt = 8;
-  
-  for (let index = 0; index < qtt; index++) {
-       compras.push(compra.toUpperCase());
-  }
-      res.json(compras);
-    });
+     var compra = "R$ 15,90";
+     var compras = [];
+     var qtt = 8;
 
-
-app.get("/fornecedor", async (req,res) =>{
-
-    var Fornecedo = "Rua joviniano de oliveira, 54 JD Carumbe";
-    var fornecedor = [];
-    var qtt = 1;
-    
-    for (let index = 0; index < qtt; index++) {
-         fornecedor.push(Fornecedo.toUpperCase());
-    }
-          res.json(fornecedor);
-        });
-
-        
-app.get("/clientes", async (req,res) =>{
-
-      var cliente = "Carlos";
-      var clientes = [];
-      var qtt = 8;
-      
-      for (let index = 0; index < qtt; index++) {
-           clientes.push(cliente.toUpperCase());
-      }
-       res.json(clientes);
-       });
+     for (let index = 0; index < qtt; index++) {
+          compras.push(compra.toUpperCase());
+     }
+     res.json(compras);
+});
 
 
+app.get("/roupas", async (req, res) => {
 
-app.get("/carros", async (req,res) =>{
+     var roupas = ["camisa","meias","calças","blusa de frio"];
+      roupas.push("cachecol");
 
-        var carro = "Corolla";
-        var carros = [];
-        var qtt = 1;
-        
-        for (let index = 0; index < qtt; index++) {
-             carros.push(carro.toUpperCase());
-        }
-         res.json(carros);
-        });
-        
-        
-app.get("/times", async (req,res) =>{
-
-          var time = "Real Madrid";
-          var times = [];
-          var qtt = 1;
-          
-          for (let index = 0; index < qtt; index++) {
-               times.push(time.toUpperCase());
-          }
-          res.json(times);
-          });
+     res.json(roupas);
+});
 
 
-app.get("/empregos", async (req,res) =>{
-           
-    var emprego = "Programador Senior";
-    var empregos = [];
-    var qtt = 1;
-  
-     for (let index = 0; index < qtt; index ++){
-           empregos.push(emprego.toLocaleUpperCase());
-      }
-        res.json(empregos);
-      });
- 
-          
-app.get("/imoveis", async (req,res) =>{
- 
-  var emprego = "Mansao Alphaville";
-  var empregos = [];4514
-  var qtt = 1;
+app.get("/fornecedor", async (req, res) => {
 
-   for (let index = 0; index < qtt; index ++){
-         empregos.push(emprego.toLocaleUpperCase());
-  
-      }
-      res.json(empregos);
-      });
-          
+     var Fornecedo = "Rua joviniano de oliveira, 54 JD Carumbe";
+     var fornecedor = [];
+     var qtt = 1;
 
-app.get("/sorteio", async (req,res) =>{
-      var sorteio = "Mega-Senna";
-      var sorteios = [];
-      var qtt = 1;
-    
-       for (let index = 0; index < qtt; index ++){
-             sorteios.push(sorteio.toLocaleUpperCase());
-      }
-       res.json(sorteios);
-      });
-               
+     for (let index = 0; index < qtt; index++) {
+          fornecedor.push(Fornecedo.toUpperCase());
+     }
+     res.json(fornecedor);
+});
 
- app.get("/linguagem", async (req,res) =>{
-       var linguage = "JavaScript";
-       var linguagem = [];
-       var qtt = 1;
-    
-        for (let index = 0; index < qtt; index ++){
-              linguagem.push(linguage.toLocaleUpperCase());
-          }
-          res.json(linguagem);
-          });
+
+app.get("/clientes", async (req, res) => {
+
+     var cliente = "Carlos";
+     var clientes = [];
+     var qtt = 8;
+
+     for (let index = 0; index < qtt; index++) {
+          clientes.push(cliente.toUpperCase());
+     }
+     res.json(clientes);
+});
+
+
+
+app.get("/carros", async (req, res) => {
+
+     var carro = "Corolla";
+     var carros = [];
+     var qtt = 1;
+
+     for (let index = 0; index < qtt; index++) {
+          carros.push(carro.toUpperCase());
+     }
+     res.json(carros);
+});
+
+
+app.get("/times", async (req, res) => {
+
+     var time = "Real Madrid";
+     var times = [];
+     var qtt = 1;
+
+     for (let index = 0; index < qtt; index++) {
+          times.push(time.toUpperCase());
+     }
+     res.json(times);
+});
+
+
+app.get("/empregos", async (req, res) => {
+
+     var emprego = "Programador Senior";
+     var empregos = [];
+     var qtt = 1;
+
+     for (let index = 0; index < qtt; index++) {
+          empregos.push(emprego.toLocaleUpperCase());
+     }
+     res.json(empregos);
+});
+
+
+app.get("/imoveis", async (req, res) => {
+
+     var emprego = "Mansao Alphaville";
+     var empregos = []; 4514
+     var qtt = 1;
+
+     for (let index = 0; index < qtt; index++) {
+          empregos.push(emprego.toLocaleUpperCase());
+
+     }
+     res.json(empregos);
+});
+
+
+app.get("/sorteio", async (req, res) => {
+     var sorteio = "Mega-Senna";
+     var sorteios = [];
+     var qtt = 1;
+
+     for (let index = 0; index < qtt; index++) {
+          sorteios.push(sorteio.toLocaleUpperCase());
+     }
+     res.json(sorteios);
+});
+
+
+app.get("/linguagem", async (req, res) => {
+     var linguage = "JavaScript";
+     var linguagem = [];
+     var qtt = 1;
+
+     for (let index = 0; index < qtt; index++) {
+          linguagem.push(linguage.toLocaleUpperCase());
+     }
+     res.json(linguagem);
+});
 
 
 
 app.listen(3000, () => {
-   console.log("estou rodando na porta 3000");
+     console.log("estou rodando na porta 3000");
 
 })
 
